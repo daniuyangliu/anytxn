d40107e0e9e02318838ccdd37c624d1f81456a10
yl <717689448@qq.com>
2020-04-03 21:55:11 +0800

    init prodect

---
+++ src/main/java/com/linktree/sikehome/YangliuApplication.java
@@ -0,0 +1,13 @@
+package com.linktree.sikehome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YangliuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YangliuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/linktree/sikehome/YangliuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linktree.sikehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YangliuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
