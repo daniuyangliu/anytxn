2ea1b8ddf99f533a4c279ce83349363e869b51cb
yl <717689448@qq.com>
2020-04-04 02:52:49 +0800

    user分库分表提交代码

--- src/main/java/com/linktree/sikehome/mapper/base/UserBaseMapper.java
+++ src/main/java/com/linktree/sikehome/mapper/base/UserBaseMapper.java
@@ -1,14 +1,18 @@
 package com.linktree.sikehome.mapper.base;
 
 import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import com.linktree.sikehome.entity.User;
 /**
 *  @author author
 */
+@Mapper
 public interface UserBaseMapper {
 
     int insertUser(User object);
+    int save(User object);
 
     int updateUser(User object);
 
--- src/test/java/com/linktree/sikehome/YangliuApplicationTests.java
+++ src/test/java/com/linktree/sikehome/YangliuApplicationTests.java
@@ -1,6 +1,8 @@
 package com.linktree.sikehome;
 
+import com.linktree.sikehome.entity.User;
 import com.linktree.sikehome.entity.address;
+import com.linktree.sikehome.mapper.base.UserBaseMapper;
 import com.linktree.sikehome.mapper.base.addressBaseMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
@@ -8,15 +10,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Date;
+
 @SpringBootTest
 @RunWith(SpringRunner.class)
 class YangliuApplicationTests {
     @Autowired
     addressBaseMapper addressBaseMapper;
+    @Autowired
+    UserBaseMapper userBaseMapper;
     @Test
     void contextLoads() {
         address address1 = address.builder().name("郭磊112XXX").code("213").pid("2133").build();
         addressBaseMapper.insertaddress(address1);
     }
-
+    @Test
+    void testUser() {
+        User user = User.builder().password("123").name("杨柳").createTime(new Date()).phone("123").cityId(5).email("213").sex(2).loginName("213").build();
+        userBaseMapper.save(user);
+    }
 }
