1e18fd8ca06ef42969559f928ca3c145c5d1cb20
yl <717689448@qq.com>
2020-04-05 00:10:15 +0800

    集成redis

--- src/main/java/com/linktree/sikehome/controller/LoginController.java
+++ src/main/java/com/linktree/sikehome/controller/LoginController.java
@@ -1,10 +1,16 @@
 package com.linktree.sikehome.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
+    @Autowired
+    private RedisTemplate<Object,Object> redisTemplate;
+
     @RequestMapping("/")
     public String login(){
         return "login";
@@ -14,4 +20,8 @@ public class LoginController {
         return "main";
     }
 
+    @RequestMapping("/redis")
+    public void redis1(){
+        redisTemplate.opsForValue().set("username","杨柳");
+    }
 }
--- src/test/java/com/linktree/sikehome/YangliuApplicationTests.java
+++ src/test/java/com/linktree/sikehome/YangliuApplicationTests.java
@@ -4,21 +4,27 @@ import com.linktree.sikehome.entity.Address;
 import com.linktree.sikehome.entity.User;
 import com.linktree.sikehome.mapper.AddressMapper;
 import com.linktree.sikehome.mapper.UserMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
+@Slf4j
 class YangliuApplicationTests {
     @Autowired
     AddressMapper addressMapper;
     @Autowired
     UserMapper userMapper;
+    @Autowired
+    RedisTemplate  redisTemplate;
     @Test
     void contextLoads() {
         Address address1 = Address.builder().name("郭磊112XXX").code("213").pid("2133").lit(2).build();
@@ -41,6 +47,16 @@ class YangliuApplicationTests {
 //        userBaseMapper.updateUser(user);
 
         System.out.println(userMapper.selectByPrimaryKey(453038159261859840L));
+    }
+    @Test
+    void testRedis(){
+        redisTemplate.opsForValue().set("username","杨柳");
+        log.info("插入成功");
+    }
+    @Test
+    void getRedis(){
+        String username = redisTemplate.opsForValue().get("username").toString();
+        log.info("redis:{}",username);
     }
 
 }
